3eb626bb0d9216743f5a639ed8d63443a5bdf385
wilson <wilson.yang@aztech.com>
2025-07-28 16:52:12 +0800

    1.修改编译脚本 2.修改NDK version = 25.3.12161346

--- android_build.sh
+++ android_build.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
 # 编译命令
-./android.sh --enable-fontconfig --disable-arm-v7a-neon
+#./android.sh --full --enable-gpl --disable-lib-srt --disable-arm-v7a --disable-arm-v7a-neon --disable-x86 --disable-x86-64
 
-echo "$1"
-if [[ "$1" == "--publishToMavenLocal" ]]; then
-  cd android
-  ./gradlew publishToMavenLocal
-fi
+./android.sh \
+--disable-arm-v7a-neon \
+--enable-fontconfig \
+--enable-x264 \
+--enable-gpl \
+--enable-android-media-codec \
+--enable-fribidi \
+--enable-gmp \
+--enable-lame \
+--enable-libtheora \
+--enable-libvorbis \
+--enable-libvpx \
+--enable-libass \
+--enable-libwebp \
+--enable-libxml2 \
+--enable-opencore-amr \
+--enable-shine \
+--enable-speex \
+--enable-dav1d \
+--enable-kvazaar \
+--enable-opus \
+--enable-twolame \
+--enable-vo-amrwbenc \
+--enable-zimg \
+--enable-snappy \
+--enable-soxr \
+--enable-gnutls \
+--enable-libilbc
